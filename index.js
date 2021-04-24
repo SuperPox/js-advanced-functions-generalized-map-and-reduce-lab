15d4a144c408e610faccab2a5169f2b802256738
SuperPox <calebkohl@gmail.com>
2021-04-24 00:26:34 -0600

    commit tree 7a4e94938d99d7f249613052bd3bff3608132bc9

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your functions here
+
+function map(arr, callback)
+{
+    const result = []
+    for (let i = 0; i < arr.length; i++)
+    {
+        const e = arr[i]
+        result.push(callback(e));
+    }
+    return result;
+}
+
+function reduce(arr, callback, startInt)
+{
+    let result;
+    if (startInt)
+    {
+        result = startInt;
+        for (let i = 0; i < arr.length; i++)
+        {
+            result = callback(arr[i], result);
+        }
+        return result;
+    }
+    else
+    {
+        result = arr[0];
+        for (let i = 0; i < arr.length; i++)
+        {
+            result = callback(arr[i], result);
+        }
+        return result;
+    }
+}
